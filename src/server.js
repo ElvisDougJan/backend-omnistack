6cba6bc956a86ee9f4df42d4d56b23266c6d9d27
Elvis Douglas Janegitz <edjelvis@gmail.com>
2019-08-06 23:46:05 -0300

    Adicionando cors

--- src/server.js
+++ src/server.js
@@ -3,10 +3,12 @@ const server = express()
 const routes = require('./routes')
 const mongoose = require('mongoose')
 const porta = process.env.PORT || 3000
+const cors = require('cors')
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-6tvwh.mongodb.net/omnistack8?retryWrites=true&w=majority', { useNewUrlParser: true })
 
 server.use(express.json())
 server.use(routes)
+server.use(cors())
 
 server.listen(porta, () => console.log(`App rodando em http://localhost:${porta}`))
