5cedd2894855f9c856dfa3de717f1d242b96ba72
Elvis Douglas Janegitz <edjelvis@gmail.com>
2019-08-06 22:10:16 -0300

    Criando server

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const server = express()
 
 server.get('/', (req, res) => {
-  res.send('API de teste')
+  return res.json({ message: `Ola ${req.query.name}` })
 })
 
 server.listen(3000, () => console.log('App rodando em http://localhost:3000'))
